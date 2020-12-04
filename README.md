8ad5608787c4f5d1d4c5c125908ec7ae3bbf75fb
Praful-rathore-222 <rathorepraful420@gmail.com>
2020-12-04 21:16:26 +0530

    Update the User logic

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# for-demo_purpose
+# for-demo_purpose# User REST API :
+This is the user authentication application by which we can be performed many API operations like user registration, login ,logout , change password, reset password and update user profile in Django Rest Framework.
+# Technology Stack :
+I have used in my application,
+
+```
+1. Python 3
+2. Django REST Framework
+3. sqlite3 Database
+4. Google Chrome v.83.0.4103.61    
+5. Google Chrome driver v.83.0.4103.61
+```
+# Project Structure :
+```
+.
+├── accounts
+│   ├── admin.py
+│   ├── apps.py
+│   ├── __init__.py
+│   ├── migrations
+│   ├── models.py
+│   ├── __pycache__
+│   ├── requirements.txt
+│   ├── tests.py
+│   └── views.py
+├── db.sqlite3
+├── manage.py
+└── projct
+    ├── __init__.py
+    ├── __pycache__
+    ├── settings.py
+    ├── urls.py
+    └── wsgi.py
+
+```
+# Running Locally
+First, clone the repository to your local machine:
+
+```
+git clone https://github.com/Praful-Rathore-222/restapi.git
+
+cd restapi
+```
+
+Install the requirements:
+
+```
+pip install -r requirements/dev.txt
+```
+Apply the database migrations:
+
+```
+python manage.py migrate
+```
+
+Load the initial data:
+``
+Create administrator/super user:
+```
+python manage.py createsuperuser 
+
+```
+
+Finally, run the development server:
+
+```
+python manage.py runserver
+```
+
+` The site will be available at 127.0.0.1:8000. `
+
+Now, open a new terminal window, Run Celery with in your project root where manage.py lives:
---
+++ projct/projct/urls.py
@@ -0,0 +1,34 @@
+"""projct URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.contrib import admin
+from rest_auth.views import PasswordResetConfirmView
+
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    
+    path('', include('django.contrib.auth.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset-complete"),
+
+]
